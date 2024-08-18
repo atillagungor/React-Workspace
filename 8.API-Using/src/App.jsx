705d22045ff514c2226689635c9df7f579122d8d
Atilla Güngör <atillaagungorr@gmail.com>
2024-08-18 14:52:50 +0300

    Axios PUT,POST,DELETE added

--- 8.API-Using/src/App.jsx
+++ 8.API-Using/src/App.jsx
@@ -16,14 +16,39 @@ function App() {
     console.log(response.data)
   }
 
+  const createUser = async (newUser) => {
+    const response = await axios.post(`${BASE_URL}/users`, newUser)
+    console.log(response.data)
+  }
+
+  const updateUser = async (userId, updatedUser) => {
+    const response = await axios.put(`${BASE_URL}/users/${userId}`, updatedUser)
+    console.log(response.data)
+  }
+
+  const deleteUserById = async (id) => {
+    const deleteResponse = await axios.delete(`${BASE_URL}/users/${id}`)
+    console.log(deleteResponse.data)
+  }
+
+  const newUser = {
+    id: 11,
+    name: "Atilla Güngör",
+    username: "Atilla",
+    email: "atillaagungorr@gmail.com",
+  }
+
   useEffect(() => {
-    // getAllUsers()
+    getAllUsers()
     getUserById(2)
+    createUser(newUser)
+    updateUser("10", { id: 10, name: "Atilla Güngör", username: "Ati", email: "atillaagungorr@gmail.com" })
+    deleteUserById("8")
   }, [])
+
 
   return (
     <>
-
     </>
   )
 }
